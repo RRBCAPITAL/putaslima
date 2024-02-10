cf9b097185413a561f2a17edd5e74f41c7346c95
RRBCAPITAL <rrbcapital24@gmail.com>
2024-02-10 10:26:36 -0500

    quitar metada

--- app/page.js
+++ app/page.js
@@ -2,31 +2,6 @@ import Head from "next/head";
 import Inicio from "@/components/Inicio/Inicio";
 import { NextSeo } from "next-seo";
 
-export const metadata = {
-  title: "Kinesiólogas y putas en Trujillo 🥰I Kinestop✅",
-  description:
-    "Anuncios de kinesiólogas reales en Perú, kinesiólogas chibolas, kinesiólogas venezolanas, colombianas, Kines teens en Lima, Lince, San Borja, Ate, Comas, Villa Maria, Villa el Salvador, Chorrillos, Miraflores",
-  keywords: [
-    "kinesiologas lima",
-    "kinesiologas en trujillo",
-    "los olivos",
-    "kines teens",
-    "kinesiologas chiclayo",
-    "prostitutas lima",
-    "kines surco",
-    "caletas lima",
-    "putas peruanas",
-    "putas extranjeras",
-    "anfitrionas lima",
-    "kines miraflores",
-    "kinesiologas venezolanas y colombianas",
-  ],
-  referrer: 'origin-when-cross-origin',
-  metadataBase: new URL('https://putastrujillo.com/'),
-  authors: [{ name: "RRB CAPITAL" }],
-  publisher: 'RRB CAPITAL',
-};
-
 export default function Home() {
   return (
     <>
